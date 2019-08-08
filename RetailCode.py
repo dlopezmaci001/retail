c9621230eb1734b14beefb322743f4cc4c23060e
Daniel Lopez <37158751+dlopezmaci001@users.noreply.github.com>
2019-08-08 09:39:27 +0200

    Update RetailCode.py

--- RetailCode.py
+++ RetailCode.py
@@ -11,6 +11,7 @@ import numpy as np
 import collections
 from collections import Counter
 import itertools
+import gensim
 from gensim.models import Word2Vec
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
@@ -22,7 +23,7 @@ import json
 # CREATE RECIPES DATASET
 
 # import recipes. (https://www.kaggle.com/hugodarwood/epirecipes)
-with open('C:/Users/Daniel/Desktop/Master/Term 3/Retail/full_format_recipes.json') as json_data:
+with open('C:/Users/daniel.lopez/Documents/GitHub/retail/full_format_recipes.json') as json_data:
     new_recipes = json.load(json_data)
     json_data.close()  
 
@@ -81,7 +82,7 @@ list_prods_in_recipes = [x for x in list_prods_in_recipes if len(x.strip())]
 display(len(list_prods_in_recipes))
 
 # Load products sold by the retailer
-products = pd.read_csv("C:/Users/Daniel/Desktop/Master/Term 3/Retail/products.csv")
+products = pd.read_csv("C:/Users/daniel.lopez/Documents/GitHub/retail/products.csv")
 
 # Transoform variables into lower case
 
@@ -163,7 +164,7 @@ def term_vectors(list_terms):
 word_vectors = term_vectors(list(product_table['prod_words']))
 
 """
-This generates a vector for each word present in the table
+Generates a vector for each word that there is in all the products.
 """
 
 # Calculate the average for the vectors produced in the previous step
@@ -192,7 +193,10 @@ def doc_vectors(word_dict,  doc_df):
         doc_w2vec[doc[1]['Key']] = np.average(doc_vector, axis=0)
     
     return doc_w2vec
-         
+"""
+Returns a dictionary with the products and the average vector based on the 
+words used in that product.
+"""         
 
 # Generate product vectors.
 # Rename the columns of the product table 
@@ -205,10 +209,6 @@ This generates a dictionary with all the average vectors for all the products
 there are available, in both of the recipes and products sold by the vendor
 """
 
-# =============================================================================
-#                                      No sé por qué se hace esto 
-# =============================================================================
-
 # create recipes dataframe
 recipes_df = pd.DataFrame({'Key':range(1, len(recipe_list)+1,1), 'ingredients':recipe_list,
                                          'directions':directions_list}) 
@@ -219,34 +219,22 @@ recipes_df = recipes_df.rename(index=str, columns={"ingredients": "Terms"})
 
 recipes_df = recipes_df[recipes_df.astype(str)['Terms'] != '[]']
 
-# Remove all rows which list contain '' term
-#recipes_df.drop(recipes_df[recipes_df['Key'] == 3680].index, inplace=True)
-#
-#recipes_df.drop(recipes_df[recipes_df['Key'] == 5210].index, inplace=True)
-#
-#recipes_df.drop(recipes_df[recipes_df['Key'] == 10797].index, inplace=True)
-
-
-# To detect where code breaks
-# for doc in recipes_df.iterrows():
-#        
-#        doc_vector = list()
-#        a = list()
-#        for term in doc[1]['Terms']:
-#            
-#            a.append(term)
-#            doc_vector.append(product_vectors[term])
-#            print(recipes_df['Key'],term)
-
-## Separate all words with a ' ' space
-#recipes_df['Terms']  = recipes_df['ingredients'].str.replace('\W', ' ')
-#
-## Split products into terms: Tokenize.
-#recipes_df['Terms'] = recipes_df['Terms'].str.split()
 #
 recipe_vectors = doc_vectors(product_vectors, recipes_df)
-#
-# =============================================================================
+"""
+For each recipe we generate a vector based on the vectors of the products sold
+"""
+
+# Play with the model
+
+model = gensim.models.Word2Vec.load("word2vec_ingredients_products.model")
+
+# Find the most similar terms 
+model.wv.most_similar('oil',topn=5)
+
+# Similarity between 2 terms
+
+model.wv.similarity(w1='pasta',w2='penne')
 
 # Creating the annoy 
 
@@ -263,6 +251,7 @@ for key,val in recipe_vectors.items():
 
 # Number of trees for queries. When making a query the more trees the easier it is to go down the right path. 
 u.build(10) # 10 trees
+
 
 # Try the annoy for the product vectors 
 
@@ -323,5 +312,119 @@ for b in similar_recipes:
 ###############################################################################
         # Create a recommender 
 ###############################################################################
+
+# Load the orders         
+orders_df = pd.read_csv("C:/Users/daniel.lopez/Documents/GitHub/retail/orders.csv") 
+
+# Visualise the graph
+orders_df['user_id'].value_counts().hist(bins=100)
+
+#Calculate the mean
+print(np.mean(orders_df['user_id'].value_counts()))
+print(np.median(orders_df['user_id'].value_counts()))
+"""
+The mean number of purchases is 17 and the median is 10
+"""
+
+# Visualise 
+orders_df['days_since_prior_order'].hist(bins=100)
+
+# Average orders
+print(np.mean(orders_df['days_since_prior_order']))
+"""
+2 to 3 orders per month
+"""
+ 
+# count group orders
+orders_count = orders_df['user_id'].value_counts().to_frame().reset_index().rename(index=str, columns={'user_id':'count'})
+print(orders_count[orders_count['count']>10]['index'].count())
+
+# Best customers
+
+orders_df[orders_df['user_id'].isin(orders_count[(orders_count['count']>30) & (orders_count['count']<32)]['index'])].count()
+
+orders_df_masked = orders_df[orders_df['user_id'].isin(orders_count[(orders_count['count']>10) & (orders_count['count']<15)]['index'])]
+print(np.mean(orders_df_masked['days_since_prior_order']))
+
+print('Number of original orders: {:,}'.format(len(orders_df_masked.order_id.unique())))
+print('Number of original orders: {:,}'.format(len(orders_df.order_id.unique())))
+
+# Masked orders combine
+
+orders_df_masked = orders_df_masked.groupby('user_id')['order_id'].apply(list)
+orders_df_masked = orders_df_masked.reset_index()#.head()
+orders_df_masked.head()
+
+# concatenate
+order_ids = list()
+for order in orders_df_masked.order_id:
+    order_ids +=order
+
+
+order_products_prior = pd.read_csv("C:/Users/daniel.lopez/Documents/GitHub/retail/order_products__prior.csv")
+
+# Baskets 
+
+baskets = order_products_prior[order_products_prior['order_id'].isin(order_ids)]
+#Group 
+
+baskets_df_masked = baskets.groupby('order_id')['product_id'].apply(list)
+
+# View 
+baskets_df_masked.head()
+
+# Create dictionary
+product_id_pair=dict()
+for product in products.iterrows():
+    product_id_pair[product[1].product_id] = product[1].product_name
+
+# create the basket vector    
+
+basket_vectors = dict()
+for basket in baskets_df_masked.iteritems(): 
+    vector_list = list()
+    
+    for product in basket[1]:
+        try:
+            if  product > 0:
+                vector_list.append(product_vectors[product_id_pair[product]])
+        except: print('you shall not pass')
         
+    basket_vectors[basket[0]]=np.average(vector_list, axis=0)  
         
+# User vector 
+user_vectors = dict()
+for user in orders_df_masked.iterrows(): 
+    vector_list = list()
+    #display(user[1])
+    for basket in user[1].order_id:
+        #display(basket_vectors[basket])
+        try:
+            vector_list.append(basket_vectors[basket])
+        except:
+            pass
+    user_vectors[user[0]]=np.average(vector_list, axis=0)    
+    
+print(list(user_vectors.items())[:3])            
+
+
+# Recommender
+
+similar_recipes = u.get_nns_by_vector(user_vectors[5], 50, search_k=-1, include_distances=False)♣
+
+# looking for list of calories in recipes dataframe
+
+from collections import Counter
+list_directions = list()
+list_ingredients=list()
+for b in similar_recipes:
+    list_directions.append(recipes_df[recipes_df.Key==b].iloc[0].directions)
+    list_ingredients.append(recipes_df[recipes_df.Key==b].iloc[0].Terms)
+
+# Create dataframe with ingredients and calories
+recipe_recommender = pd.DataFrame({'Recepe_Ingredients':list_ingredients,'Directions':list_directions})
+
+# Order data using the min and max number of calories specified by the customer
+min_max=[250,500]
+recipe_recommender[recipe_recommender.Calories>min_max[0]][recipe_recommender.Calories<=min_max[1]].sort_values('Calories',ascending=False)
+
